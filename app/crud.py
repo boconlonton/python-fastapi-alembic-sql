2f588e8d48e31c4c4fe7879e2c4bd5a13d22548d
prd-tan-truong <tan.truong@paradox.ai>
2022-08-19 14:01:42 +0700

    Add endpoint

---
+++ app/crud.py
@@ -0,0 +1,66 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import update
+from fastapi import HTTPException
+import hashlib
+from app import models, schemas
+
+hash_key = hashlib.sha256()
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).get(user_id)
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hash_key.update(user.password.encode("utf-8"))
+    hashed_password = hash_key.hexdigest()
+
+    db_user = models.User(email=user.email, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_articles(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Article).offset(skip).limit(limit).all()
+
+
+def create_article(db: Session, article: schemas.ArticleCreate, user_id: int):
+    db_item = models.Article(**article.dict(), author_id=user_id)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+
+def get_object_or_404(db: Session, Model: models.Base, object_id: int):
+    db_object = db.query(Model).filter(Model.id == object_id).first()
+    if db_object is None:
+        raise HTTPException(status_code=404, detail="Not found")
+    return db_object
+
+
+def update_article(db: Session, article_id: int, updated_fields: schemas.ArticleUpdate):
+    db.execute(
+        update(models.Article)
+        .where(models.Article.id == article_id)
+        .values(updated_fields.dict(exclude_unset=True))
+    )
+
+    db.flush()
+    db.commit()
+    return updated_fields
+
+
+def delete_article(db: Session, article: schemas.Article):
+    db.delete(article)
+    db.commit()
---
+++ app/main.py
@@ -0,0 +1,82 @@
+from typing import List
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+app = FastAPI()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@app.get("/articles/", response_model=List[schemas.Article])
+def read_articles(skip: int = 0, limit: int = 100,
+                  db: Session = Depends(get_db)):
+    articles = crud.get_articles(db, skip=skip, limit=limit)
+    return articles
+
+
+@app.get("/articles/{article_id}", response_model=schemas.Article)
+def read_article(article_id: int, db: Session = Depends(get_db)):
+    return crud.get_object_or_404(db, models.Article, object_id=article_id)
+
+
+@app.post(
+    "/users/{user_id}/articles/",
+    response_model=schemas.Article,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_user_article(
+        user_id: int, article: schemas.ArticleCreate,
+        db: Session = Depends(get_db)
+):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User does not exist")
+    return crud.create_article(db=db, article=article, user_id=user_id)
+
+
+@app.delete("/articles/{article_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_article(article_id: int, db: Session = Depends(get_db)):
+    article = crud.get_object_or_404(db, models.Article, article_id)
+    return crud.delete_article(db=db, article=article)
+
+
+@app.patch("/articles/{article_id}", response_model=schemas.ArticleUpdate)
+def update_article(
+        article_id: int,
+        updated_fields: schemas.ArticleUpdate,
+        db: Session = Depends(get_db),
+):
+    return crud.update_article(db, article_id, updated_fields)
